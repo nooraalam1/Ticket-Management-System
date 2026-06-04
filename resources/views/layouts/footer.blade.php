7a6acd4c052f1a89b8d7877e8856054d09757f37
nooraalam1 <noorsomrat2000@gmail.com>
2026-06-04 16:25:29 +0600

    privacy policy modal

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,25 +1,163 @@
+<style>
+    .text-center {
+        text-align: center;
+        font-size: 20px;
+    }
+</style>
+
+
 <div class="text-white flex justify-between items-center">
     <div class="">
         <h3>Powered By: <span class="">Noor A Alam</span></h3>
     </div>
     <div class="">
-        <h3><button onclick="openModal('terms')">Terms and Conditions</button> | <button onclick="openModal('privacy')">Privacy Policy</button></h3>
+        <h3><button onclick="openModal('terms')">Terms and Conditions</button> | <button
+                onclick="openModal('privacy')">Privacy Policy</button></h3>
     </div>
     <div class="">
-        <img src="{{asset('logo.png')}}" alt="logo" srcset="" width="80px">
+        <img src="{{ asset('logo.png') }}" alt="logo" srcset="" width="80px">
     </div>
 </div>
+
+<button onclick="closeModal('terms','privacy')" id="modalOverlay" class="hidden fixed"
+    style="z-index:1 ;color:white; background-color: red;padding:20px ;right:18%; top:5%">X</button>
 
 
 {{-- Terms Modal --}}
-<div id="terms" class="hidden fixed top-0 ">
-   <h2 class="text-2xl font-bold mb-4 text-white ">Terms & Conditions</h2>
+<div id="terms" class="hidden fixed bg-white"
+    style="top:7%; left:50%;transform:translateX(-50%); padding:15px; overflow-y:auto;max-height:85vh ">
+    <h3 class="text-dark text-center" style="font-size:30px;">Terms & Conditions</h3>
+    <p>1. Users must provide accurate information during registration.</p>
+    <p>2. Tickets are subject to availability.</p>
+    <p>3. Unauthorized access or misuse of the system is strictly prohibited.</p>
+    <p>4. We are not responsible for delays or cancellations caused by external factors.</p>
+    <p>5. Terms may be updated anytime without prior notice.</p>
+    <br>
+    <span class="text-center" style="color:red">Collection of Personally Identifiable Information and other
+        Information:</span> <br>
+    <p style="text-align: justify">
+        When you use our Website, we store your browsing information so that we can provide services and features that
+        meet your needs.In general, you can browse the Website without telling us who you are or revealing any
+        personal information
+        about yourself. Once you give us your personal information, you are not anonymous to us. You always have the
+        option to not provide information by choosing not to use a particular service or feature on the Website. We
+        compile your usage behavior and personal information and the information on an aggregate basis for internal
+        research to better enhance our product offerings to serve you better. This information may include the URL that
+        you just came from (whether this URL is on our Website or not), which URL you next go to (whether this URL is on
+        our Website or not), your computer browser information, and your IP address. <br> We use data collection devices
+        such as "cookies (small file stored on your computer)" on certain pages of the Website to help analyze our web
+        page flow, measure promotional effectiveness, and promote trust and safety. We offer certain features that are
+        only available through the use of a "cookie".<br>Additionally, third parties may also place cookies or similar
+        devices on our Website, which we cannotcontrol.If you choose to buy on the Website, we collect information about
+        your buying behavior. <br>If you transact with us, we collect some additional information, such as a billing
+        address, a credit/debit card
+        number and a credit/debit card expiration date and/ or other payment instrument details. If you post messages or
+        leave feedback for us, we will collect that information you provide to us. We retain this information as
+        necessary to resolve disputes, provide customer support and troubleshoot problems as permitted by law. <br>If
+        you send us personal correspondence, such as emails or letters, or if other users or third parties send us
+        correspondence about your activities or postings on the Website, we may collect such information into a file
+        specific to you. <br>We collect personally identifiable information (email address, name, phone number, NID)
+        from you when you set up a free account with us. However, data protection is a matter of trust and your privacy
+        is important to us. We shall therefore use your name and other information which relates to you in the manner
+        set out in this Privacy Policy. We will only collect information where it is necessary for us to do so and we
+        will only collect
+        information
+    </p>
+    <br><br>
+    <span class="text-center" style="color:red">Sharing of personal information:</span> <br>
+    <p style="text-align: justify">
+
+        We will only share personal information with companies, organizations or individuals outside the periphery of
+        Bangladesh Railway and Shohoz-Synesis-Vincen JV if we have a good-faith and believe that access, use,
+        preservation or disclosure of the information is reasonably necessary to:
+
+        Meet any applicable law, regulation, legal process or enforceable governmental request. enforce applicable Terms
+        of Service, including investigation of potential violations, detect, prevent, or otherwise address fraud,
+        security or technical issues. protect against harm to the rights, property or safety of Bangladesh Railway and
+        Shohoz-Synesis-Vincen JV, our users or the public as required or permitted by law.
+
+        We may share aggregated, non-personally identifiable information publicly and with our partners – like bus
+        operators, agents or connected sites. For example, we may share information publicly to show trends about the
+        general use of our services. We may also share consolidated information provided by like-minded users 3rd
+        parties without ever taking individual names, email ids or other contact details.
+
+        If Bangladesh Railway and Shohoz-Synesis-Vincen JV is involved in a merger, acquisition or asset sale, we will
+        continue to ensure the confidentiality of your personal information and give notice before personal information
+        is transferred or becomes subject to a different privacy policy.
+    </p>
+</div>
+{{-- Privacy Modal --}}
+<div id="privacy" class="hidden fixed bg-white"
+    style="top:7%; left:50%;transform:translateX(-50%); padding:15px; overflow-y:auto;max-height:85vh ">
+    <h3 class="text-dark text-center" style="font-size:30px;">Privacy Policy</h3>
+    <p>1. Users must provide accurate information during registration.</p>
+    <p>2. Tickets are subject to availability.</p>
+    <p>3. Unauthorized access or misuse of the system is strictly prohibited.</p>
+    <p>4. We are not responsible for delays or cancellations caused by external factors.</p>
+    <p>5. Terms may be updated anytime without prior notice.</p>
+    <br>
+    <span class="text-center" style="color:red">Collection of Personally Identifiable Information and other
+        Information:</span> <br>
+    <p style="text-align: justify">
+        When you use our Website, we store your browsing information so that we can provide services and features that
+        meet your needs.In general, you can browse the Website without telling us who you are or revealing any
+        personal information
+        about yourself. Once you give us your personal information, you are not anonymous to us. You always have the
+        option to not provide information by choosing not to use a particular service or feature on the Website. We
+        compile your usage behavior and personal information and the information on an aggregate basis for internal
+        research to better enhance our product offerings to serve you better. This information may include the URL that
+        you just came from (whether this URL is on our Website or not), which URL you next go to (whether this URL is on
+        our Website or not), your computer browser information, and your IP address. <br> We use data collection devices
+        such as "cookies (small file stored on your computer)" on certain pages of the Website to help analyze our web
+        page flow, measure promotional effectiveness, and promote trust and safety. We offer certain features that are
+        only available through the use of a "cookie".<br>Additionally, third parties may also place cookies or similar
+        devices on our Website, which we cannotcontrol.If you choose to buy on the Website, we collect information about
+        your buying behavior. <br>If you transact with us, we collect some additional information, such as a billing
+        address, a credit/debit card
+        number and a credit/debit card expiration date and/ or other payment instrument details. If you post messages or
+        leave feedback for us, we will collect that information you provide to us. We retain this information as
+        necessary to resolve disputes, provide customer support and troubleshoot problems as permitted by law. <br>If
+        you send us personal correspondence, such as emails or letters, or if other users or third parties send us
+        correspondence about your activities or postings on the Website, we may collect such information into a file
+        specific to you. <br>We collect personally identifiable information (email address, name, phone number, NID)
+        from you when you set up a free account with us. However, data protection is a matter of trust and your privacy
+        is important to us. We shall therefore use your name and other information which relates to you in the manner
+        set out in this Privacy Policy. We will only collect information where it is necessary for us to do so and we
+        will only collect
+        information
+    </p>
+    <br><br>
+    <span class="text-center" style="color:red">Sharing of personal information:</span> <br>
+    <p style="text-align: justify">
+
+        We will only share personal information with companies, organizations or individuals outside the periphery of
+        Bangladesh Railway and Shohoz-Synesis-Vincen JV if we have a good-faith and believe that access, use,
+        preservation or disclosure of the information is reasonably necessary to:
+
+        Meet any applicable law, regulation, legal process or enforceable governmental request. enforce applicable Terms
+        of Service, including investigation of potential violations, detect, prevent, or otherwise address fraud,
+        security or technical issues. protect against harm to the rights, property or safety of Bangladesh Railway and
+        Shohoz-Synesis-Vincen JV, our users or the public as required or permitted by law.
+
+        We may share aggregated, non-personally identifiable information publicly and with our partners – like bus
+        operators, agents or connected sites. For example, we may share information publicly to show trends about the
+        general use of our services. We may also share consolidated information provided by like-minded users 3rd
+        parties without ever taking individual names, email ids or other contact details.
+
+        If Bangladesh Railway and Shohoz-Synesis-Vincen JV is involved in a merger, acquisition or asset sale, we will
+        continue to ensure the confidentiality of your personal information and give notice before personal information
+        is transferred or becomes subject to a different privacy policy.
+    </p>
 </div>
 
 <script>
-    function openModal(id){
+    function openModal(id) {
         document.getElementById(id).classList.remove('hidden')
+        document.getElementById('modalOverlay').classList.remove('hidden')
+    }
+
+    function closeModal(id) {
+        document.getElementById(id).classList.add('hidden')
+        document.getElementById('modalOverlay').classList.add('hidden')
     }
 </script>
-
-
