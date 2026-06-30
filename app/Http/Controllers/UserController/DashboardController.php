c3732e860e05476b8566283e745c46a68479f2c6
nooraalam1 <noorsomrat2000@gmail.com>
2026-06-30 19:03:54 +0600

    user dashboard

--- app/Http/Controllers/UserController/DashboardController.php
+++ app/Http/Controllers/UserController/DashboardController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\UserController;
 use App\Http\Controllers\Controller;
+use App\Models\Station;
 
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('dashboard');
+        $stations = Station::orderBy('name','ASC')->get();
+        return view('dashboard',compact('stations'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,8 +3,29 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100 text-center">
-                   Home
+                    Home
                 </div>
+                <form action="#">
+                    <label>From</label>
+                    <select name="from" class="form-control select select2">
+                        <option value="">Select Station</option>
+                        @foreach ($stations as $station)
+                            <option>{{ $station->name }}</option>
+                        @endforeach
+                    </select>
+                    <label>To</label>
+                    <select name="to" class="form-control select select2">
+                        <option value="">Select Station</option>
+                        @foreach ($stations as $station)
+                            <option>{{ $station->name }}</option>
+                        @endforeach
+                    </select>
+
+                        <label>Date</label>
+                        <input type="date" name="date" class="form-control">
+
+
+                </form>
             </div>
         </div>
     </div>
